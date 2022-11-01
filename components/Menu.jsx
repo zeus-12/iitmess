9f7c9c84856c7e707bfc8cfccb8257d1dc21871e
Vishnu <vishnu@Vishnus-MacBook-Pro.local>
2022-11-01 22:18:22 +0530

    fixed colour bug

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -5,17 +5,17 @@ const Menu = ({ data, weekNo, day }) => {
     data[meal].map((food) => <li key={food}>{food}</li>);
 
   const meals = [
-    { meal: "breakfast", colour: "orange-200" },
-    { meal: "lunch", colour: "green-300" },
-    { meal: "dinner", colour: "blue-300" },
+    { meal: "breakfast", colour: "text-orange-200" },
+    { meal: "lunch", colour: "text-green-300" },
+    { meal: "dinner", colour: "text-blue-300" },
   ];
-
   return (
     <div>
       <div className="flex justify-evenly">
         {meals.map((item) => (
           <div key={item.meal}>
-            <p className={`text-${item.colour} capitalize`}>{item.meal}</p>
+            {/* {console.log(`text-${item.colour}`)} */}
+            <p className={`${item.colour} capitalize`}>{item.meal}</p>
             <ul className="list-disc px-2">{menuList(item.meal)}</ul>
           </div>
         ))}
